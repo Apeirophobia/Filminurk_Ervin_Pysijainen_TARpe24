eccf959091175210354dae9c4c1089d4922fc93b
Apeirophobia <ervinpusijainen@gmail.com>
2025-10-30 09:35:22 +0200

    Created ActorsController, added Constructor and Index method

---
+++ Filminurk/Filminurk/Controllers/ActorsController.cs
@@ -0,0 +1,35 @@
+﻿using Filminurk.Data;
+using Filminurk.Models.Actors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filminurk.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly FilminurkTARpe24Context _context;
+        // private readonly IActorServices _actorServices;
+        public ActorsController
+            (
+            FilminurkTARpe24Context context
+            )
+        {
+            _context = context;
+        }
+
+        
+        public IActionResult Index()
+        {
+            var result = _context.Actors.Select(x => new ActorsIndexViewModel()
+            {
+                ActorID = x.ActorID,
+                FirstName = x.FirstName,
+                LastName = x.LastName,
+                MoviesActedFor = x.MoviesActedFor,
+                FavouriteGenre = x.FavouriteGenre,
+                HasAwards = x.HasAwards
+            });
+
+            return View(result);
+        }
+    }
+}
