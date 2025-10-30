16b4636ad74abdfac954db2ba1d2d30f22b11b76
Apeirophobia <ervinpusijainen@gmail.com>
2025-10-30 10:34:06 +0200

    Created ActorServices and IActorServices interface
    Created ActorServices and IActorServices. Small fix: renamed method from "Add" to "Create"

---
+++ Filminurk/Filminurk.ApplicationServices/Services/ActorServices.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Filminurk.Core.Domain;
+using Filminurk.Core.Dto;
+using Filminurk.Core.ServiceInterface;
+using Filminurk.Data;
+
+namespace Filminurk.ApplicationServices.Services
+{
+    public class ActorServices : IActorServices
+    {
+        private readonly FilminurkTARpe24Context _context;
+
+        public ActorServices(FilminurkTARpe24Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<Actor> Create(ActorsDTO dto)
+        {
+            Actor actor = new Actor();
+            actor.ActorID = Guid.NewGuid();
+            actor.FirstName = dto.FirstName;
+            actor.LastName = dto.LastName;
+            actor.NickName = dto.NickName;
+            actor.MoviesActedFor = dto.MoviesActedFor;
+            actor.FavouriteGenre = dto.FavouriteGenre;
+            actor.HasAwards = dto.HasAwards;
+            actor.EntryCreatedAt = DateTime.Now;
+            actor.EntryModifiedAt = DateTime.Now;
+
+            await _context.Actors.AddAsync(actor);
+            await _context.SaveChangesAsync();
+
+            return actor;
+        }
+    }
+}
