e65042d4c1cae292391b27dffbb5c4e531fc86eb
Apeirophobia <ervinpusijainen@gmail.com>
2025-10-30 10:41:45 +0200

    Added Create method to interface, added interface to controller, finished POST method
    Added Create method to IActorsServices
    Added IActorsServices to ActorsController
    Finished writing Create Post method in ActorsController

--- Filminurk/Filminurk.Core/ServiceInterface/IActorServices.cs
+++ Filminurk/Filminurk.Core/ServiceInterface/IActorServices.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Filminurk.Core.Domain;
+using Filminurk.Core.Dto;
 
 namespace Filminurk.Core.ServiceInterface
 {
     public interface IActorServices
     {
+        Task<Actor> Create(ActorsDTO dto);
 
     }
 }
--- Filminurk/Filminurk/Controllers/ActorsController.cs
+++ Filminurk/Filminurk/Controllers/ActorsController.cs
@@ -1,4 +1,6 @@
-﻿using Filminurk.Core.Dto;
+﻿using Filminurk.ApplicationServices.Services;
+using Filminurk.Core.Dto;
+using Filminurk.Core.ServiceInterface;
 using Filminurk.Data;
 using Filminurk.Models.Actors;
 using Microsoft.AspNetCore.Mvc;
@@ -8,13 +10,15 @@ namespace Filminurk.Controllers
     public class ActorsController : Controller
     {
         private readonly FilminurkTARpe24Context _context;
-        // private readonly IActorServices _actorServices;
+        private readonly IActorServices _actorServices;
         public ActorsController
             (
-            FilminurkTARpe24Context context
+            FilminurkTARpe24Context context,
+            IActorServices actorServices
             )
         {
             _context = context;
+            _actorServices = actorServices;
         }
 
         
@@ -59,7 +63,14 @@ namespace Filminurk.Controllers
                     EntryModifiedAt = vm.EntryModifiedAt
                 };
                 var result = await _actorServices.Create(dto);
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                return RedirectToAction(nameof(Index));
+                
             }
+            return RedirectToAction(nameof(Index));
         }
     }
 }
