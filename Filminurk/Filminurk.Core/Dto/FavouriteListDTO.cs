a0763d7a3d7855381349a0ba756a0e72c88a7874
Apeirophobia <ervinpusijainen@gmail.com>
2025-11-20 10:10:25 +0200

    Working on FavouriteLists Views and Controller

--- Filminurk/Filminurk.Core/Dto/FavouriteListDTO.cs
+++ Filminurk/Filminurk.Core/Dto/FavouriteListDTO.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Filminurk.Core.Domain;
+using Microsoft.AspNetCore.Http;
 
 namespace Filminurk.Core.Dto
 {
@@ -21,9 +22,14 @@ namespace Filminurk.Core.Dto
         // public List<Actor>? ListOfActors { get; set; }
 
         /* andmebaasiomadused */
+        public List<IFormFile> Files { get; set; }
+        public IEnumerable<FileToDatabaseDTO> Iamge { get; set; } = new List<FileToDatabaseDTO>();
+        
         public DateTime ListCreateAt { get; set; }
         public DateTime ListModifiedAt { get; set; }
         public DateTime ListDeletedAt { get; set; }
         public bool IsReported { get; set; } = false;
+
+        
     }
 }
