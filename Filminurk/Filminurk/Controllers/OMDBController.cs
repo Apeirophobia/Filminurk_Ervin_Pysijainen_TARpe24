b5c57cacc8def04ca4a4f1740eeb9c26e85af7c2
Apeirophobia <ervinpusijainen@gmail.com>
2026-01-26 12:27:24 +0200

    Created OMDB Controller, Index, FindMovie Post and Movie Get methods (incomplete)

---
+++ Filminurk/Filminurk/Controllers/OMDBController.cs
@@ -0,0 +1,39 @@
+﻿using Filminurk.Core.Dto.OMDB;
+using Filminurk.Models.OMDB;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filminurk.Controllers
+{
+    public class OMDBController : Controller
+    {
+        
+        public OMDBController()
+        {
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult FindMovie(OMDBSearchViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Movie", "OMDB", new {movieName = vm.MovieTitle});
+            }
+
+            return View("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Movie(string movieName)
+        {
+            OMDBSearchResultDTO dto = new();
+            dto.Title = movieName;
+
+            return View();
+        }
+    }
+}
