d6ddbe01c843de3a55dc7a541f15b4b97118cc63
Apeirophobia <ervinpusijainen@gmail.com>
2026-01-26 12:28:59 +0200

    Created Service with Interface (incomplete)

---
+++ Filminurk/Filminurk.ApplicationServices/Services/OMDBServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Filminurk.Core.ServiceInterface;
+
+namespace Filminurk.ApplicationServices.Services
+{
+    public class OMDBServices : IOMDBServices
+    {
+
+    }
+}
