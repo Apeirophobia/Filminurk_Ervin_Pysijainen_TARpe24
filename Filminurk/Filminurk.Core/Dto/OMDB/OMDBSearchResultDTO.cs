ec126aa8a64990c9186a2fc5ba6b1651c128ad5d
Apeirophobia <ervinpusijainen@gmail.com>
2026-01-26 12:28:42 +0200

    Created OMDB SearchResultDTO

---
+++ Filminurk/Filminurk.Core/Dto/OMDB/OMDBSearchResultDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace Filminurk.Core.Dto.OMDB
+{
+    public class OMDBSearchResultDTO
+    {
+        public string? Title { get; set; }
+        public string? Year { get; set; }
+        public string? Rated { get; set; }
+        public string? Released { get; set; }
+        public string? Runtime { get; set; }
+        public string? Genre { get; set; }
+        public string? Director { get; set; }
+    }
+}
