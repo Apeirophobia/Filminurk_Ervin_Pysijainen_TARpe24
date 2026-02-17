50b34e477a444618561195938242432756090494
Apeirophobia <ervinpusijainen@gmail.com>
2026-02-17 17:05:02 +0200

    Created a ChatController for ChatHub implementation.
    Registered SignInManager
    Created Methods:
    Constructor for dependency injection
    Index -> sends signed in user into index view

---
+++ Filminurk/Filminurk/Controllers/ChatController.cs
@@ -0,0 +1,23 @@
+﻿using Filminurk.Core.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filminurk.Controllers
+{
+    public class ChatController : Controller
+    {
+        private readonly SignInManager<Filminurk.Core.Domain.ApplicationUser> _signInManager;
+
+        public ChatController(SignInManager<Filminurk.Core.Domain.ApplicationUser> signInManager)
+        {
+            _signInManager= signInManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.myUserName = await _signInManager.UserManager.GetUserAsync(User);
+            return View();
+        }
+
+        
+    }
+}
