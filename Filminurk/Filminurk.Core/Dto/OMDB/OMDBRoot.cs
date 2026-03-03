79104f62f64672eb30fc84df633cabca4c97286d
Apeirophobia <ervinpusijainen@gmail.com>
2026-03-03 15:48:27 +0200

    Created Root DTO for OMDB

---
+++ Filminurk/Filminurk.Core/Dto/OMDB/OMDBRoot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filminurk.Core.Dto.OMDB
+{
+    public class OMDBRoot
+    {
+        public string? Title { get; set; }
+        public string? Year { get; set; }
+        public string? Rated { get; set; }
+        public string? Released { get; set; }
+        public string? Runtime { get; set; }
+        public string? Genre { get; set; }
+        public string? Director { get; set; }
+        public string? imdbID { get; set; }
+    }
+}
