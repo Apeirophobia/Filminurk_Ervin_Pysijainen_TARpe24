095688c0bc9ba9e4ec4e489b07e00f7837eebe43
Apeirophobia <ervinpusijainen@gmail.com>
2026-03-03 15:48:09 +0200

    Created interface for OMDB services

--- Filminurk/Filminurk.Core/ServiceInterface/IOMDBServices.cs
+++ Filminurk/Filminurk.Core/ServiceInterface/IOMDBServices.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Filminurk.Core.Dto.OMDB;
 
 namespace Filminurk.Core.ServiceInterface
 {
     public interface IOMDBServices
     {
+        public Task<OMDBSearchResultDTO> OMDBResult(OMDBSearchResultDTO dto);
     }
 }
